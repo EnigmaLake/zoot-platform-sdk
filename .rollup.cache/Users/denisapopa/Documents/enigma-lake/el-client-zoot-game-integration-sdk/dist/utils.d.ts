507bef766ae7ffcdb018ca6b461947ab29109a74
denisa <popa.denisa31@gmail.com>
2024-03-18 16:07:11 +0200

    gitignore

--- .rollup.cache/Users/denisapopa/Documents/enigma-lake/el-client-zoot-game-integration-sdk/dist/utils.d.ts
+++
@@ -1,8 +0,0 @@
-import { Currency } from ".";
-export declare const getCurrentBalance: ({ balance, currency, }: {
-    balance: {
-        sweepsBalance: number;
-        goldBalance: number;
-    };
-    currency: Currency;
-}) => number;
--- .rollup.cache/Users/denisapopa/Documents/enigma-lake/el-client-zoot-game-integration-sdk/dist/utils.js
+++
@@ -1,7 +0,0 @@
-import { Currency } from ".";
-export const getCurrentBalance = ({ balance, currency, }) => {
-    return currency === Currency.GOLD
-        ? balance?.goldBalance ?? 0
-        : balance?.sweepsBalance ?? 0;
-};
-//# sourceMappingURL=utils.js.map
